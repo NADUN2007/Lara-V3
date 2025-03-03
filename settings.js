4abf18dae7c5e1680ac937adfc36387ffe32be5c
NADUN2007 <159978957+NADUN2007@users.noreply.github.com>
2025-03-03 20:36:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "SjQ3wZoA#K1GIbMdalxalN7jE2JsUDo7POF2r24h7K2DFUrKAW3A", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
